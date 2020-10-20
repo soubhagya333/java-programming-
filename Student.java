86676398ea35281c73ca49b1294d5e1bd4fbf058
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-20 09:58:51 +0530

    Add files via upload
    This is program for student details

---
+++ Student.java
@@ -0,0 +1,9 @@
+class Student{
+String name;
+String branch;
+String college;
+String gender;
+void info() {
+System.out.println(name + " "+ "is an engineer"+".And "+gender +" "+"is graduated from" +" "+ college+" with stream of" +" "+ branch+".");
+}
+}
