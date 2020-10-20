a62cecdcb5d186401b506a1a3c49cc1ac54ca259
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-20 10:00:54 +0530

    Add files via upload
    this is program for studentInfo

---
+++ StudentInfo.java
@@ -0,0 +1,40 @@
+class StudentInfo{
+
+public static void main (String args[]){
+
+Student soubhagya  =  new Student();
+soubhagya.name = "soubhagya ";
+soubhagya.branch= "ECE";
+soubhagya.college="Bgmit mudhol";
+soubhagya.gender="she";
+soubhagya.info();
+
+Student suma  =  new Student();
+suma.name = "suma";
+suma.branch = "ECE";
+suma.college = "Skit benglore";
+suma.gender="she";
+suma.info();
+
+Student kushala =  new Student();
+kushala.name = "kushla";
+kushala.branch = "ECE";
+kushala.college = "Bgsit mandya";
+kushala.gender="she";
+kushala.info();
+
+Student supriya  =  new Student();
+supriya.name = "supriya";
+supriya.branch = "ECE";
+supriya.college = "Bgmit mudhol";
+supriya.gender="she";
+supriya.info();
+
+Student sharnu  =  new Student();
+sharnu.name = "sharnu";
+sharnu.branch = "ECE";
+sharnu.college = "Bgmit mudhol";
+sharnu.gender="he";
+sharnu.info();
+}
+}
