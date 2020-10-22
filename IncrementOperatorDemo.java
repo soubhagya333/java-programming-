327e850083b6eee881e3abe7dd856f3c93900594
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-22 19:34:37 +0530

    Add files via upload
    this is program for increment

---
+++ IncrementOperatorDemo.java
@@ -0,0 +1,16 @@
+class IncrementOperatorDemo{
+
+public static void main(String args[]){
+
+int x= 5;
+int y=4;
+int z=++x;
+System.out.println(++z);
+System.out.println(++y);
+System.out.println(++x);
+System.out.println(++z);
+System.out.println(y);
+System.out.println(x);
+System.out.println(z);
+}
+}
