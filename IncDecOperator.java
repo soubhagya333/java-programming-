14377b7a80c26f0f50c04703c088bf674aaec995
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-22 21:23:25 +0530

    Add files via upload
    this is program for increment and decrement oprator

---
+++ IncDecOperator.java
@@ -0,0 +1,15 @@
+class IncDecOperator{
+
+public static void main(String args[]){
+
+int x= 8;
+int y=5;
+int z=--y;//4
+System.out.println(++y);
+System.out.println(z);
+System.out.println(--y);
+System.out.println(--x);
+System.out.println(++z);
+System.out.println(y);
+}
+}
