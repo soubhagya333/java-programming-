e8394cbefe5b677784196b42f47f0fc2f844f7a9
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-22 12:06:27 +0530

    Add files via upload
    second example of decrement operator

---
+++ decrementopratord.java
@@ -0,0 +1,15 @@
+class decrementopratord{
+
+public static void main(String args[]){
+
+int x= 50;
+int y=40;
+int z=--x;
+System.out.println(--y);
+System.out.println(--x);
+System.out.println(--y);
+System.out.println(y--);
+System.out.println(y);
+System.out.println(z);
+}
+}
