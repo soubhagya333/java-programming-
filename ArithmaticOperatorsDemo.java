f864452bc41581f952de66fc87bfb4c2e778cdbf
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-22 19:32:44 +0530

    Add files via upload
    athis is program for arithmatic oprator

---
+++ ArithmaticOperatorsDemo.java
@@ -0,0 +1,32 @@
+class ArithmaticOperatorsDemo{
+
+public static void main(String args[]){
+
+short n1 = 10;
+short n2 = 20;
+int n3 =n1+ n2;
+System.out.println("n1+n2 ="+n3); //30
+
+int i1 = 20;
+int i2 = 10;
+int i3 = i1- i2;
+System.out.println("i1-i2 ="+i3); //10
+
+int k1 = 20;
+int k2 = 10;
+int k3 = k1 * k2;
+System.out.println("k1*k2 ="+k3); //2
+
+short l1 = 20;
+int l2 = 10;
+int l3 = l1 / l2;
+System.out.println("l1/l2 ="+l3); //10
+
+int x1 = 20;
+short x2 = 10;
+int x3 = x1 % x2;
+System.out.println("x1%x2 ="+x3); //10
+
+
+}
+}
