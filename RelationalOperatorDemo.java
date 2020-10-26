9f3049d199bc0cbd2f4a69de52936fa4fc2dee8b
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-26 20:24:06 +0530

    Add files via upload
    this is the Relational operator program

---
+++ RelationalOperatorDemo.java
@@ -0,0 +1,14 @@
+class RelationalOperatorDemo{
+
+public static void main(String[] args){
+
+int x = 50 , y = 60 , z = 60;
+
+System.out.println("x   == y  : " +  (x == y));
+System.out.println("x   !=  y : "+ (x != y));
+System.out.println("x   < y :"+  (x <y));
+System.out.println("y  <= z :"+ (y <=z));
+System.out.println("y   > z : " +  (y >=z));
+
+}
+}
