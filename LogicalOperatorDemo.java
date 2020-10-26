a9298bb38d9b698887c55956fdce66829b695327
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-26 20:43:25 +0530

    Add files via upload
    this is logical operator program

---
+++ LogicalOperatorDemo.java
@@ -0,0 +1,13 @@
+class LogicalOperatorDemo{
+
+public static void main(String args[]){
+
+int x = 10, y = 20;
+
+System.out.println(" (x==y)  && (x<y): "+ ((x==y)  && (x++ <y)));
+System.out.println(" x : " +x);
+System.out.println(" (x==y)  || (x<y): "+ ((x==y)  || (x++ <y)));
+System.out.println(" x : " +x);
+
+}
+}
