9ee55d313eaad948a15b1e3a3939a896667fa495
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-26 20:28:37 +0530

    Add files via upload
    this is program for IfCondition .

---
+++ IfConditionDemo.java
@@ -0,0 +1,14 @@
+class IfConditionDemo{
+
+public static void main(String args[]){
+
+System.out.println("main started....");
+
+int age = 20;
+
+if(age == 20){
+System.out.println("my age is 18");
+}
+System.out.println("main ended...");
+}
+}
