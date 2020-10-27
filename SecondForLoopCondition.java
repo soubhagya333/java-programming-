f9cd13d62bcbc696c698334db56e9c273974d61a
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-27 21:31:58 +0530

    Add files via upload
    This is program  for pattern of 1-9 numbers in matrix(3*3)

---
+++ SecondForLoopCondition.java
@@ -0,0 +1,13 @@
+class SecondForLoopCondition{
+public static void main(String[] args){
+       int p = 3,q = 1;
+
+for(int i =1; i <= p ; i++){
+for(int j =1; j <= p; j++){
+System.out.print(q +" ");
+q++;
+}
+System.out.println();
+ }
+}
+}
