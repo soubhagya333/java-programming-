60d50f7253311a03a5901dd70b069a8d0493f1d8
soubhagya333 <72903741+soubhagya333@users.noreply.github.com>
2020-10-28 15:21:56 +0530

    Add files via upload
    This is program for breakstatement.

---
+++ BreakStatementDemo.java
@@ -0,0 +1,15 @@
+class BreakStatementDemo{
+
+public static void main(String args[]){
+
+for (int i=0; i<5; i++){
+      if(i ==3){
+         System.out.println("breaking the loop at 3");
+break;
+}
+
+      System.out.println(i);
+}
+
+}
+}
